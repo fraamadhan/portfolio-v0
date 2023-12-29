b18c8e1b5e570af0fb68060a3d66925cd96beb23
fraamadhan <fakhrifajarrr@gmail.com>
2023-12-29 10:26:29 +0700

    feat: main page done

---
+++ public/js/script.js
@@ -0,0 +1,29 @@
+(() => {
+  "use strict";
+
+  const forms = document.querySelectorAll(".needs-validation");
+
+  Array.from(forms).forEach((form) => {
+    form.addEventListener(
+      "submit",
+      (event) => {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+
+        form.classList.add("was-validated");
+      },
+      false
+    );
+  });
+})();
+
+//TO-DO
+/*
+
+send email
+move to details
+display all my projects in details page
+static I provide the data through JSON
+ */
