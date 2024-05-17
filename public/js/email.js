9e1d5d24fe5c44150831125e32621308cd72a03a
fraamadhan <fakhrifajarrr@gmail.com>
2024-05-17 14:50:34 +0700

    async function email:

--- public/js/email.js
+++ public/js/email.js
@@ -1,28 +1,29 @@
-function sendEmail() {
+(function () {
+  emailjs.init({
+    publicKey: "8Jbep9TKhLqvizhPB",
+  });
+})();
+
+async function sendEmail() {
   const name = $("#name").val();
   const email = $("#email").val();
   const message = $("#message").val();
 
-  if (name != "" && email != "" && message != "") {
+  if (name && email && message) {
     const data = {
       from_name: name,
       email_sender: email,
       message: message,
     };
-    console.log("name", NAME);
-    console.log("message", message);
-    console.log("email", email);
 
-    emailjs.send("service_l12oorv", "template_8buijhj", data).then(
-      (response) => {
-        console.log("SUCCESS", response.status, response.text);
-        alert("Email sent successfully!");
-      },
-      (error) => {
-        alert("Failed to send the email");
-        console.log("ERROR", error);
-      }
-    );
+    try {
+      const response = await emailjs.send("service_l12oorv", "template_8buijhj", data);
+      console.log("SUCCESS", response.status, response.text);
+      alert("Email sent successfully!");
+    } catch (error) {
+      console.log("ERROR", error);
+      alert("Failed to send the email");
+    }
   } else {
     alert("Please input correctly");
   }
--- public/js/init.js
+++
@@ -1,5 +0,0 @@
-(function () {
-  emailjs.init({
-    publicKey: "8Jbep9TKhLqvizhPB",
-  });
-});
