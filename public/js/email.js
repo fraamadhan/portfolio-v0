8084b5c3e1ec397ad89cdd96e4540a6a6ae35300
fraamadhan <fakhrifajarrr@gmail.com>
2024-05-17 14:08:46 +0700

    simple validation input

--- public/js/email.js
+++ public/js/email.js
@@ -8,21 +8,24 @@ function sendEmail() {
   const name = $("#name").val();
   const email = $("#email").val();
   const message = $("#message").val();
-  console.log("NAME", name);
 
-  const data = {
-    from_name: name,
-    email_sender: email,
-    message: message,
-  };
+  if (name != "" && email != "" && message != "") {
+    const data = {
+      from_name: name,
+      email_sender: email,
+      message: message,
+    };
 
-  emailjs.send("service_l12oorv", "template_8buijhj", data).then(
-    (response) => {
-      console.log("SUCCESS", response.status, response.text);
-      alert("Email sent successfully!");
-    },
-    (error) => {
-      console.log("FAILED", error);
-    }
-  );
+    emailjs.send("service_l12oorv", "template_8buijhj", data).then(
+      (response) => {
+        console.log("SUCCESS", response.status, response.text);
+        alert("Email sent successfully!");
+      },
+      (error) => {
+        console.log("FAILED", error);
+      }
+    );
+  } else {
+    alert("Please input correctly");
+  }
 }
